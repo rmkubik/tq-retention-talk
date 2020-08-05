cf1af7303c181b527b34ccc66b7ffcceff564161
Ryan Kubik <rkubik@twilio.com>
2020-08-05 16:01:43 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Test from "./test.mdx";
+
+const App = () => {
+  return <Test />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
